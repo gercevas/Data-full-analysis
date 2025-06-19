a7ab856a83e365515d4d6ca8a9b12712afbbbc0b
Germán Cevallos <gcevallosmeneses@gmail.com>
2025-06-19 16:10:07 +0200

    Configuración archivo .gitignore y setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="temporizador",
+    name="Hojas_excel",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[],
     author="German Andres Cevallos",
     author_email="germanandrescevallos@gmail.com",
-    description="Una librería para manejo de tiempos",
+    description="Librería para el análisis de censos ganaderos y cálculo de emisiones",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/gercevas/Proyecto_git.git",
+    url="https://github.com/gercevas/Data-full-analysis.git",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 ) 

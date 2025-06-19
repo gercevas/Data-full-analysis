38cb9f43668f15ae2154b16206b2823c607f545f
Germán Cevallos <gcevallosmeneses@gmail.com>
2025-06-19 17:42:19 +0200

    Configuracion de archivos

--- setup.py
+++ setup.py
@@ -5,7 +5,14 @@ setup(
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=[],
+    install_requires=[
+        'numpy>=2.3.0',
+        'pandas>=2.3.0',
+        'python-dateutil>=2.9.0',
+        'pytz>=2025.2',
+        'tzdata>=2025.2',
+        'openpyxl>=3.1.5',
+    ],
     author="German Andres Cevallos",
     author_email="germanandrescevallos@gmail.com",
     description="Librería para el análisis de censos ganaderos y cálculo de emisiones",
@@ -17,5 +24,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.6",
 ) 

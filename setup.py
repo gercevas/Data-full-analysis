9ef246a36738700caac5bef9746d93321ae07e66
Germán Cevallos <gcevallosmeneses@gmail.com>
2025-06-19 13:29:36 +0200

    Creación de carpetas iniciales

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="temporizador",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[],
+    author="German Andres Cevallos",
+    author_email="germanandrescevallos@gmail.com",
+    description="Una librería para manejo de tiempos",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/gercevas/Proyecto_git.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+) 
